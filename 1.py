232625c42995a948243d474ca06a16492d6e8b7f
Aden King <adenking@live.com>
2019-07-30 15:21:41 +1000

    fix up SQRT for lower values and format better

--- 1.py
+++ 1.py
@@ -8,10 +8,10 @@ for i in range(2, 1000000):
     else:
         print(len(prime_numbers))
     prime = True
-    for j in range(2, int(math.sqrt(i))):
-        if( i % j == 0):
+    for j in range(2, max(int(math.sqrt(i)), min(i, 5))):
+        if i % j == 0:
             prime = False
-    if(prime is True):
+    if prime is True:
         prime_numbers.append(i)
 
 print(prime_numbers[10000])
